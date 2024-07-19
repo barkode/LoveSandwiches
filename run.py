547c8571db911ca47ab8a72504a741736dfc2f0e
Kostiantyn Krysenko <konstantinks@gmail.com>
2024-07-19 10:50:06 +0000

    Add function that show quantity of sandwiches needed

--- run.py
+++ run.py
@@ -130,7 +130,19 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
+print("Welcome to Love Sandwiches Data Automation.\n")
+stock_data = main()
 
-print("Welcome to Love Sandwiches Data Automation")
-main()
+def get_stock_values(data):
+    '''
+    Get stock values from the worksheet.
+    '''
+    headings = SHEET.worksheet("stock").row_values(1)
+    dct = {key:el for (key, el) in zip(headings, data)}
+    return dct
+
+stock_values = get_stock_values(stock_data)
+print("Make the following numbers of sandwiches to the next market:\n")
+print(stock_values)
